670513c4514279fdaf501455c621670e3cea0a75
Stefan Bezo <stefan.bezo@gmail.com>
2024-11-13 16:32:37 +0100

    scenario 3 - kubernetes

--- S1-4_routers_in_a_row/main.tf
+++ S1-4_routers_in_a_row/main.tf
@@ -34,41 +34,46 @@ data "vsphere_virtual_machine" "CSR1000V" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+data "vsphere_virtual_machine" "Alpine" {
+  name          = "SB-Alpine-Template"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
 #####################################################################################
 
 ### Links ###
 
-# Create a new distributed port group - Link-A
-resource "vsphere_distributed_port_group" "LINK-A" {
-  name               = "VC-DPG-VLAN1160-LINK-A"
-  vlan_id            = 1160
+# Create a new distributed port group - Link-AA
+resource "vsphere_distributed_port_group" "LINK-AA" {
+  name               = "VC-DPG-VLAN2160-LINK-AA"
+  vlan_id            = 2160
   distributed_virtual_switch_uuid = data.vsphere_distributed_virtual_switch.dvs.id
 }
 
-# Create a new distributed port group - Link-B
-resource "vsphere_distributed_port_group" "LINK-B" {
-  name               = "VC-DPG-VLAN1161-LINK-B"
-  vlan_id            = 1161
+# Create a new distributed port group - Link-BB
+resource "vsphere_distributed_port_group" "LINK-BB" {
+  name               = "VC-DPG-VLAN2161-LINK-BB"
+  vlan_id            = 2161
   distributed_virtual_switch_uuid = data.vsphere_distributed_virtual_switch.dvs.id
 }
 
-# Create a new distributed port group - Link-C
-resource "vsphere_distributed_port_group" "LINK-C" {
-  name               = "VC-DPG-VLAN1162-LINK-C"
-  vlan_id            = 1162
+# Create a new distributed port group - Link-CC
+resource "vsphere_distributed_port_group" "LINK-CC" {
+  name               = "VC-DPG-VLAN2162-LINK-CC"
+  vlan_id            = 2162
   distributed_virtual_switch_uuid = data.vsphere_distributed_virtual_switch.dvs.id
 }
 
 ### Routers ###
 
-# create router R1
-resource "vsphere_virtual_machine" "R1" {
- name             = "SB-CSR1000V-R1"
+# create linux Alpine11
+resource "vsphere_virtual_machine" "A11" {
+ name             = "SB-Alpine-11"
  resource_pool_id = data.vsphere_resource_pool.pool.id
  datastore_id     = data.vsphere_datastore.datastore.id
  num_cpus = 1
  memory   = 4096
- guest_id = data.vsphere_virtual_machine.CSR1000V.guest_id
+ guest_id = data.vsphere_virtual_machine.Alpine.guest_id
  folder = "USER_VMs/SB"
  wait_for_guest_net_timeout = 0
 
@@ -77,18 +82,19 @@ resource "vsphere_virtual_machine" "R1" {
    adapter_type = "vmxnet3"
  }
   network_interface {
-   network_id   = vsphere_distributed_port_group.LINK-A.id
+   network_id   = vsphere_distributed_port_group.LINK-AA.id
    adapter_type = "vmxnet3"
  }
  disk {
    label = "Hard disk 1"
-   size             = 8
+   size             = 16
    thin_provisioned = true
  }
  clone {
-   template_uuid = data.vsphere_virtual_machine.CSR1000V.id
+   template_uuid = data.vsphere_virtual_machine.Alpine.id
  }
 }
+
 
 # create router R2
 resource "vsphere_virtual_machine" "R2" {
@@ -106,11 +112,11 @@ resource "vsphere_virtual_machine" "R2" {
    adapter_type = "vmxnet3"
  }
   network_interface {
-   network_id   = vsphere_distributed_port_group.LINK-A.id
+   network_id   = vsphere_distributed_port_group.LINK-AA.id
    adapter_type = "vmxnet3"
  }
    network_interface {
-   network_id   = vsphere_distributed_port_group.LINK-B.id
+   network_id   = vsphere_distributed_port_group.LINK-BB.id
    adapter_type = "vmxnet3"
  }
  disk {
@@ -139,11 +145,11 @@ resource "vsphere_virtual_machine" "R3" {
    adapter_type = "vmxnet3"
  }
   network_interface {
-   network_id   = vsphere_distributed_port_group.LINK-B.id
+   network_id   = vsphere_distributed_port_group.LINK-BB.id
    adapter_type = "vmxnet3"
  }
    network_interface {
-   network_id   = vsphere_distributed_port_group.LINK-C.id
+   network_id   = vsphere_distributed_port_group.LINK-CC.id
    adapter_type = "vmxnet3"
  }
  disk {
@@ -156,14 +162,14 @@ resource "vsphere_virtual_machine" "R3" {
  }
 }
 
-# create router R4
-resource "vsphere_virtual_machine" "R4" {
- name             = "SB-CSR1000V-R4"
+# create linux Alpine12
+resource "vsphere_virtual_machine" "A12" {
+ name             = "SB-Alpine-12"
  resource_pool_id = data.vsphere_resource_pool.pool.id
  datastore_id     = data.vsphere_datastore.datastore.id
  num_cpus = 1
  memory   = 4096
- guest_id = data.vsphere_virtual_machine.CSR1000V.guest_id
+ guest_id = data.vsphere_virtual_machine.Alpine.guest_id
  folder = "USER_VMs/SB"
  wait_for_guest_net_timeout = 0
 
@@ -172,44 +178,15 @@ resource "vsphere_virtual_machine" "R4" {
    adapter_type = "vmxnet3"
  }
   network_interface {
-   network_id   = vsphere_distributed_port_group.LINK-C.id
+   network_id   = vsphere_distributed_port_group.LINK-CC.id
    adapter_type = "vmxnet3"
  }
  disk {
    label = "Hard disk 1"
-   size             = 8
+   size             = 16
    thin_provisioned = true
  }
  clone {
-   template_uuid = data.vsphere_virtual_machine.CSR1000V.id
- }
-}
-
-# create router R5
-resource "vsphere_virtual_machine" "R5" {
- name             = "SB-CSR1000V-R5"
- resource_pool_id = data.vsphere_resource_pool.pool.id
- datastore_id     = data.vsphere_datastore.datastore.id
- num_cpus = 1
- memory   = 4096
- guest_id = data.vsphere_virtual_machine.CSR1000V.guest_id
- folder = "USER_VMs/SB"
- wait_for_guest_net_timeout = 0
-
- network_interface {
-   network_id   = data.vsphere_network.MNG.id
-   adapter_type = "vmxnet3"
- }
-  network_interface {
-   network_id   = vsphere_distributed_port_group.LINK-C.id
-   adapter_type = "vmxnet3"
- }
- disk {
-   label = "Hard disk 1"
-   size             = 8
-   thin_provisioned = true
- }
- clone {
-   template_uuid = data.vsphere_virtual_machine.CSR1000V.id
+   template_uuid = data.vsphere_virtual_machine.Alpine.id
  }
 }
---
+++ S3-kubernetes_cluster/main.tf
@@ -0,0 +1,112 @@
+provider "vsphere" {
+  user           = var.vc_user
+  password       = var.vc_password
+  vsphere_server = var.vc_server
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = var.vc_dc
+}
+
+data "vsphere_distributed_virtual_switch" "dvs" {
+  name          = var.vc_dvs
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = var.vc_pool
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vc_datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "MNG" {
+  name          = var.vc_mng
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "SB-Ubuntu-Template" {
+  name          = "SB-Ubuntu-Template"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+#####################################################################################
+
+
+# create kubernetes node SB-K8Node-01
+resource "vsphere_virtual_machine" "SB-K8Node-01" {
+  name             = "SB-K8Node-01"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus = 2
+  memory   = 4096
+  guest_id = data.vsphere_virtual_machine.SB-Ubuntu-Template.guest_id
+  folder = "USER_VMs/SB"
+  wait_for_guest_net_timeout = 0  
+  network_interface {
+    network_id   = data.vsphere_network.MNG.id
+    adapter_type = "vmxnet3"
+  } 
+  disk {
+    label = "Hard disk 1"
+    size             = 32
+    thin_provisioned = true
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.SB-Ubuntu-Template.id
+    customize {
+      linux_options {
+        host_name = "SB-K8Node-01"
+        domain    = "lab.local"
+      }
+      network_interface {
+        ipv4_address = "10.208.116.120"
+        ipv4_netmask = 24
+      }
+
+      ipv4_gateway = "10.208.116.1"
+    }
+  }
+}
+
+# create kubernetes node SB-K8Node-02
+resource "vsphere_virtual_machine" "SB-K8Node-02" {
+  name             = "SB-K8Node-02"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus = 2
+  memory   = 4096
+  guest_id = data.vsphere_virtual_machine.SB-Ubuntu-Template.guest_id
+  folder = "USER_VMs/SB"
+  wait_for_guest_net_timeout = 0  
+  network_interface {
+    network_id   = data.vsphere_network.MNG.id
+    adapter_type = "vmxnet3"
+  } 
+  disk {
+    label = "Hard disk 1"
+    size             = 32
+    thin_provisioned = true
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.SB-Ubuntu-Template.id
+    customize {
+      linux_options {
+        host_name = "SB-K8Node-02"
+        domain    = "lab.local"
+      }
+      network_interface {
+        ipv4_address = "10.208.116.121"
+        ipv4_netmask = 24
+      }
+
+      ipv4_gateway = "10.208.116.1"
+    }
+  }
+}
